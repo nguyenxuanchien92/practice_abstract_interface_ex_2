dd94bb74548c7bd3afd89bf2c5de6c60f431df61
nguyenxuanchien92 <nguyenxuanchien92@gmail.com>
2020-11-14 16:47:55 +0700

    done

---
+++ src/entities/Circle.java
@@ -0,0 +1,50 @@
+package entities;
+
+public class Circle {
+    private double radius;
+    private String color;
+    private boolean filled;
+
+    public Circle() {
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(double radius, String color, boolean filled) {
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                ", color='" + color + '\'' +
+                ", filled=" + filled +
+                '}';
+    }
+}
